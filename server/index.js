a46e72b4532458ca8fce7dc9fa8a3c3f4fa6ff32
umarkhon <umarkhonakhmedov2000@gmail.com>
2023-03-23 13:02:46 +0500

    Connecting database

--- server/index.js
+++ server/index.js
@@ -22,4 +22,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 // Routes
-app.use("/client", client);
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+app.use("/management", managementRoutes);
+app.use("/sales", salesRoutes);
+
+// Mongoose
+const PORT = process.env.PORT || 9000;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => consolelog(`${error} did not connect`));
