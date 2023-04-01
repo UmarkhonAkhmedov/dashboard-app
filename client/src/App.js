c0888ab606e6cd02708bb4a6dab3aa9daa126ab8
umarkhon <umarkhonakhmedov2000@gmail.com>
2023-04-01 08:21:39 +0500

    Creating Layout and dashbord components

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { themeSettings } from "theme";
 import { useSelector } from "react-redux";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "scenes/dashboard";
+import Layout from "scenes/layout";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -10,9 +13,17 @@ function App() {
 
   return (
     <div className="App">
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
